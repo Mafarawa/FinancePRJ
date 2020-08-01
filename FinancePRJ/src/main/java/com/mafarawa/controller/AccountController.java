bfa48f18807621d9573a52ba85217dc1d03e9e91
Mafarawa <valeraplahotnik397@gmail.com>
2020-08-02 02:43:45 +0300

    for handling account operations like add, remove, edit, etc...

---
+++ FinancePRJ/src/main/java/com/mafarawa/controller/AccountController.java
@@ -0,0 +1,58 @@
+package com.mafarawa.controller;
+
+import com.mafarawa.view.AccountView;
+import com.mafarawa.model.AccountModel;
+import com.mafarawa.connect.DBGate;
+
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+import java.util.ArrayList;
+import java.sql.*;
+import org.apache.log4j.Logger;
+
+public class AccountController extends AccountView {
+	private int account_id;
+
+	private static Logger logger;
+	static { logger = Logger.getLogger(AccountController.class.getName()); }
+
+	public AccountController(String name) {
+		super();
+
+		getUserAccountsList(name);
+
+		super.accountList.setOnMouseClicked(e -> getSelectedAccount(super.accountList.getSelectionModel().getSelectedItem()));
+	}
+
+	private void getSelectedAccount(String accountName) {
+		DBGate dbGate = DBGate.getInstance();
+
+		try {
+			ResultSet rs = dbGate.executeData("SELECT account.type, account.balance FROM account WHERE account.name=" + "'" + accountName + "';");
+			while(rs.next()) {
+				super.accountNameValue.setText(accountName);
+				super.accountTypeValue.setText(rs.getString(1));
+				super.accountBalanceValue.setText(String.valueOf(rs.getInt(2)));
+			}
+		} catch(Exception e) {
+			logger.error("Exception: ", e);
+		}
+	}
+
+	private void getUserAccountsList(String name) {
+		DBGate dbGate = DBGate.getInstance();
+
+		try {
+			ResultSet rs = dbGate.executeData("SELECT userfp.account FROM userfp WHERE userfp.name=" + "'" + name + "';");
+			rs.next();
+			this.account_id = rs.getInt("account");
+
+			rs = dbGate.executeData("SELECT account.name FROM account JOIN userfp ON userfp.id=" + account_id + ";");
+			while(rs.next()) {
+				super.accountList.getItems().add(rs.getString("name"));
+			}
+		} catch(Exception e) {
+			logger.error("Exception: ", e);
+		}
+	}
+}
