8ddf8c67451560ffff2bffdff202df04bdf4c2ac
Mafarawa <valeraplahotnik397@gmail.com>
2020-08-26 01:19:06 +0300

    removed repiting code

--- FinancePRJ/src/main/java/com/mafarawa/view/authreg/SelectImageView.java
+++ FinancePRJ/src/main/java/com/mafarawa/view/authreg/SelectImageView.java
@@ -10,52 +10,35 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import java.util.ArrayList;
 
 public class SelectImageView {
+    protected ArrayList<Button> buttons;
     protected Stage childStage;
-    protected Button blackUserButton;
-    protected Button blueUserButton;
-    protected Button greenUserButton;
-    protected Button redUserButton;
-    protected Button whiteUserButton;
-    protected Button yellowUserButton;
     protected Scene scene;
 
     public SelectImageView(Stage stage) {
-        blackUserButton = new Button("", new ImageView(
-                new Image(getClass().getResourceAsStream(UserImage.getImageById(1)), 100, 100, false, false)
-        ));
-
-        blueUserButton = new Button("", new ImageView(
-                new Image(getClass().getResourceAsStream(UserImage.getImageById(2)), 100, 100, false, false)
-        ));
-
-        greenUserButton = new Button("", new ImageView(
-                new Image(getClass().getResourceAsStream(UserImage.getImageById(3)), 100, 100, false, false)
-        ));
-
-        redUserButton = new Button("", new ImageView(
-                new Image(getClass().getResourceAsStream(UserImage.getImageById(4)), 100, 100, false, false)
-        ));
-
-        whiteUserButton = new Button("", new ImageView(
-                new Image(getClass().getResourceAsStream(UserImage.getImageById(5)), 100, 100, false, false)
-        ));
-        
-        yellowUserButton = new Button("", new ImageView(
-                new Image(getClass().getResourceAsStream(UserImage.getImageById(6)), 100, 100, false, false)
-        ));
+        buttons = new ArrayList<>();
+        buttons.add(new Button(""));
+        buttons.add(new Button(""));
+        buttons.add(new Button(""));
+        buttons.add(new Button(""));
+        buttons.add(new Button(""));
+        buttons.add(new Button(""));
 
         GridPane selectImageLayout = new GridPane();
         selectImageLayout.setHgap(20);
         selectImageLayout.setVgap(20);
         selectImageLayout.setAlignment(Pos.CENTER);
-        selectImageLayout.add(blackUserButton, 0, 0);
-        selectImageLayout.add(blueUserButton, 1, 0);
-        selectImageLayout.add(greenUserButton, 0, 1);
-        selectImageLayout.add(redUserButton, 1, 1);
-        selectImageLayout.add(whiteUserButton, 0, 2);
-        selectImageLayout.add(yellowUserButton, 1, 2);
+
+        int i = buttons.size() - 1;
+        for(int row = 0; row <= 2; row++) {
+            for(int column = 0; column <= 1; column++) {
+                buttons.get(i).setGraphic(new ImageView(new Image(getClass().getResourceAsStream(UserImage.getImageById(i)), 100, 100, false, false)));            
+                selectImageLayout.add(buttons.get(i), column, row);
+                i--;
+            }
+        }
 
         scene = new Scene(selectImageLayout, 300, 400);
 
