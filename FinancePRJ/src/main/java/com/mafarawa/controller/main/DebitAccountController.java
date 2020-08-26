a5d585c61f616c0a41f234e868fe18670d059acc
Mafarawa <valeraplahotnik397@gmail.com>
2020-08-27 01:00:04 +0300

    transactions now are safety

--- FinancePRJ/src/main/java/com/mafarawa/controller/main/DebitAccountController.java
+++ FinancePRJ/src/main/java/com/mafarawa/controller/main/DebitAccountController.java
@@ -11,19 +11,15 @@ import java.sql.*;
 public class DebitAccountController extends DebitAccountView {
 	private int id;
 
-	private static DBGate dbGate;
     private static Logger logger;
-
-    static {
-    	dbGate = DBGate.getInstance();
-    	logger = Logger.getLogger(DebitAccountController.class.getName());
-    }
+    static { logger = Logger.getLogger(DebitAccountController.class.getName()); }
 
 	public DebitAccountController(Stage stage, String accountName, String username) {
 		super(stage);
 
 		logger.debug("Debit account: " + accountName);
 
+		DBGate dbGate = DBGate.getInstance();
     	try {
     		ResultSet rs = dbGate.executeData("SELECT userfp.id FROM userfp WHERE userfp.name='" + username +"';");
     		rs.next();
@@ -42,6 +38,7 @@ public class DebitAccountController extends DebitAccountView {
 
 	// This method used to execute accounts in order to display them
 	private void getAccountList(String accountName) {
+		DBGate dbGate = DBGate.getInstance();
 		try {
 			ResultSet rs = dbGate.executeData("SELECT account.name " + 
 											  "FROM account " + 
@@ -58,6 +55,7 @@ public class DebitAccountController extends DebitAccountView {
 
 	// This method used to execute expances in order to display them
 	private void getExpanceList() {
+		DBGate dbGate = DBGate.getInstance();
 		try {
 			ResultSet rs = dbGate.executeData("SELECT expance.category " +
 											  "FROM expance " + 
@@ -68,37 +66,88 @@ public class DebitAccountController extends DebitAccountView {
 		} catch(Exception e) {
 			logger.error("Exception: ", e);
 		}
+
 	}
 
 	// This method used to transfer money from the first account to the second account
 	private void transactionFromAccount(String accountName) {
+		DBGate dbGate = DBGate.getInstance();
+
 		String target = super.accountList.getSelectionModel().getSelectedItem();
 		int sum = Integer.parseInt(super.accountInput.getText());
-		String transaction = "BEGIN;" +
-							 "UPDATE account SET balance = balance - " + sum + " WHERE name='" + accountName + "';" +
-							 "UPDATE account SET balance = balance + " + sum + " WHERE name='" + target + "';" +
-							 "COMMIT;";
 
 		try {
-			// dbGate.transaction(transaction);
-	    	super.childStage.fireEvent(new WindowEvent(super.childStage, WindowEvent.WINDOW_CLOSE_REQUEST));			
+			// Increase balance of selected account
+			PreparedStatement accountAddition = dbGate.getDatabase().prepareStatement("UPDATE account " +
+																					  "SET balance = balance - ? " +
+																					  "WHERE name = ?;");
+			accountAddition.setInt(1, sum);
+			accountAddition.setString(2, accountName);
+			dbGate.insertData(accountAddition);
+
+			// Decrease balance of selected account
+			PreparedStatement accountSubstraction = dbGate.getDatabase().prepareStatement("UPDATE account " + 
+																					  	  "SET balance = balance + ? " +
+																					   	  "WHERE name = ?;");
+			accountSubstraction.setInt(1, sum);
+			accountSubstraction.setString(2, target);
+			dbGate.insertData(accountSubstraction);
+
+			// Document transaction
+			PreparedStatement writeTransaction = dbGate.getDatabase().prepareStatement("INSERT INTO transactions (transaction_id, from_point, action, amount, to_point, transaction_date) " +
+																					   "VALUES (?, ?, ?, ?, ?, ?);");
+			writeTransaction.setInt(1, this.id);
+			writeTransaction.setString(2, accountName);
+			writeTransaction.setInt(3, 2);
+			writeTransaction.setInt(4, sum);
+			writeTransaction.setString(5, target);
+			writeTransaction.setObject(6, super.accountDatePicker.getValue());
+			dbGate.insertData(writeTransaction);
+
 		} catch(Exception e) {
+			try { dbGate.getDatabase().rollback(); } catch(Exception r) { logger.error("Exception: ", r); }
 			logger.error("Exception: ", e);
 		}
+
+		try { dbGate.getDatabase().commit(); } catch(Exception e) { logger.error("Exception: ", e); }
+    	super.childStage.fireEvent(new WindowEvent(super.childStage, WindowEvent.WINDOW_CLOSE_REQUEST));			
+
+    	logger.info("Transaction completed");
 	}
 
 	// This method used to transfer money from the account to the selected expance category
 	private void transactionFromExpance(String accountName) {
+		DBGate dbGate = DBGate.getInstance();
+
+		String expance = super.expanceList.getSelectionModel().getSelectedItem();
 		int sum = Integer.parseInt(super.expanceInput.getText());
-		String transaction = "BEGIN;" +
-							 "UPDATE account SET balance = balance - " + sum + " WHERE name='" + accountName + "';" +
-							 "COMMIT;";
 
 		try {
-			// dbGate.transaction(transaction);
-	    	super.childStage.fireEvent(new WindowEvent(super.childStage, WindowEvent.WINDOW_CLOSE_REQUEST));			
+			PreparedStatement accountAddition = dbGate.getDatabase().prepareStatement("UPDATE account " + 
+																					  "SET balance = balance - ? " + 
+																					  "WHERE name = ?;");
+			accountAddition.setInt(1, sum);
+			accountAddition.setString(2, accountName);
+			dbGate.insertData(accountAddition);
+
+			PreparedStatement writeTransaction = dbGate.getDatabase().prepareStatement("INSERT INTO transactions (transaction_id, from_point, action, amount, to_point, transaction_date) " +
+																					   "VALUES (?, ?, ?, ?, ?, ?);");
+			writeTransaction.setInt(1, this.id);
+			writeTransaction.setString(2, expance);
+			writeTransaction.setInt(3, 2);
+			writeTransaction.setInt(4, sum);
+			writeTransaction.setString(5, accountName);
+			writeTransaction.setObject(6, super.expanceDatePicker.getValue());
+			dbGate.insertData(writeTransaction);
+
 		} catch(Exception e) {
+			try { dbGate.getDatabase().rollback(); } catch(Exception r) { logger.error("Exception: ", r); }
 			logger.error("Exception: ", e);
 		}
+
+		try { dbGate.getDatabase().commit(); } catch(Exception e) { logger.error("Exception: ", e); }		
+    	super.childStage.fireEvent(new WindowEvent(super.childStage, WindowEvent.WINDOW_CLOSE_REQUEST));			
+
+    	logger.info("Transaction completed");
 	}
 }
