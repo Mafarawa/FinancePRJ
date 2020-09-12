e57980aa8fab8d03bb380d9b3c2aae9eb30598bd
Mafarawa <valeraplahotnik397@gmail.com>
2020-09-13 02:24:58 +0300

    Model for displaying in table

---
+++ FinancePRJ/src/main/java/com/mafarawa/model/TransactionModel.java
@@ -0,0 +1,28 @@
+package com.mafarawa.model;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class TransactionModel {
+	private SimpleStringProperty fromPoint;
+	private SimpleStringProperty action;
+	private SimpleIntegerProperty amount;
+	private SimpleStringProperty toPoint;
+
+	public TransactionModel(String fromPoint, String action, int amount, String toPoint) {
+		this.fromPoint = new SimpleStringProperty(fromPoint);
+		this.action = new SimpleStringProperty(action);
+		this.amount = new SimpleIntegerProperty(amount);
+		this.toPoint = new SimpleStringProperty(toPoint);
+	}
+
+	public void setFromPoint(String fromPoint) { this.fromPoint.set(fromPoint); }
+	public void setAction(String action) { this.action.set(action); }
+	public void setAmount(Integer amount) { this.amount.set(amount); }
+	public void setToPoint(String toPoint) { this.toPoint.set(toPoint); }
+
+	public String getFromPoint() { return this.fromPoint.get(); }
+	public String getAction() { return this.action.get(); }
+	public Integer getAmount() { return this.amount.get(); }
+	public String getToPoint() { return this.toPoint.get(); }
+}
