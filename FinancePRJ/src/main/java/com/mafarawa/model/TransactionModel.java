9ee5afe02b684019b33bd2b4d257d531081a82b7
Mafarawa <valeraplahotnik397@gmail.com>
2020-09-14 22:19:56 +0300

    Added date

--- FinancePRJ/src/main/java/com/mafarawa/model/TransactionModel.java
+++ FinancePRJ/src/main/java/com/mafarawa/model/TransactionModel.java
@@ -8,21 +8,25 @@ public class TransactionModel {
 	private SimpleStringProperty action;
 	private SimpleIntegerProperty amount;
 	private SimpleStringProperty toPoint;
+	private SimpleStringProperty date;
 
-	public TransactionModel(String fromPoint, String action, int amount, String toPoint) {
+	public TransactionModel(String fromPoint, String action, int amount, String toPoint, String date) {
 		this.fromPoint = new SimpleStringProperty(fromPoint);
 		this.action = new SimpleStringProperty(action);
 		this.amount = new SimpleIntegerProperty(amount);
 		this.toPoint = new SimpleStringProperty(toPoint);
+		this.date = new SimpleStringProperty(date);
 	}
 
 	public void setFromPoint(String fromPoint) { this.fromPoint.set(fromPoint); }
 	public void setAction(String action) { this.action.set(action); }
 	public void setAmount(Integer amount) { this.amount.set(amount); }
 	public void setToPoint(String toPoint) { this.toPoint.set(toPoint); }
+	public void setDate(String date) { this.date.set(date); }
 
 	public String getFromPoint() { return this.fromPoint.get(); }
 	public String getAction() { return this.action.get(); }
 	public Integer getAmount() { return this.amount.get(); }
 	public String getToPoint() { return this.toPoint.get(); }
+	public String getDate() { return this.date.get(); }
 }
