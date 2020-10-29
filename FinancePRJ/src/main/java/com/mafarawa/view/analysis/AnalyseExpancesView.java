1dbd67ad59da7f4b0605386700dc266fa99abc72
Mafarawa <valeraplahotnik397@gmail.com>
2020-10-29 20:18:27 +0200

    move graph to the center

--- FinancePRJ/src/main/java/com/mafarawa/view/analysis/AnalyseExpancesView.java
+++ FinancePRJ/src/main/java/com/mafarawa/view/analysis/AnalyseExpancesView.java
@@ -2,18 +2,16 @@ package com.mafarawa.view.analysis;
 
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SplitPane;
+import javafx.scene.layout.HBox;
+import javafx.geometry.Pos;
 
 public class AnalyseExpancesView {
-	private SplitPane rootLayout;
-	protected ListView<String> categoryList;
+	private HBox rootLayout;
 	protected NumberAxis xAxis;
 	protected NumberAxis yAxis;
 	protected LineChart<Number, Number> lineChart;
 
 	public AnalyseExpancesView() {
-		categoryList = new ListView<>();
 		xAxis = new NumberAxis();
 		yAxis = new NumberAxis();
 		lineChart = new LineChart<>(xAxis, yAxis);
@@ -22,9 +20,9 @@ public class AnalyseExpancesView {
 		yAxis.setLabel("Баланс");
 		lineChart.setTitle("Анализ расходов");
 
-		rootLayout = new SplitPane(categoryList, lineChart);
-		rootLayout.setDividerPositions(0.3);		
+		rootLayout = new HBox(lineChart);
+		rootLayout.setAlignment(Pos.CENTER);
 	}
 
-	public SplitPane getLayout() { return this.rootLayout; }
+	public HBox getLayout() { return this.rootLayout; }
 }
